5db0f0328d229d2f4e48c90c14e8df470972340d
Nika Nozadze <nikanozadze1010@gmail.com>
2023-06-28 22:52:16 +0400

    define final project functions

---
+++ final/high_or_low.py
@@ -0,0 +1,36 @@
+import random
+
+combination = ["2","3","4","5","6","7","8","9","10", "J", "Q", "K", "T"]
+
+def main():
+    score = 0
+    start = random_cards()
+    print(f"card: {combination[start]}")
+    while True:
+        random_element_index = random_cards()
+        result = check(start, random_element_index)
+        if(result):
+            score += 1
+            start = random_element_index
+        else:
+            break
+    print(f"your score is: {score}")
+
+def check(start, next):
+    user = user_choice()
+    return (user and next > start) or (not user and next < start)
+
+
+def random_cards():
+    random_element = random.choice(combination)
+    random_index = combination.index(random_element)
+    return random_index
+
+def user_choice():
+    user_input = False
+    while user_input != "high" and user_input != "low":
+        user_input = input("high or low ? ").lower()
+    return user_input == "high"
+
+if __name__ == "__main__":
+    main()
